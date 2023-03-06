340ff00e5341f9027a5f1bdef97f39e024fd60c6
pratt <prattnj@gmail.com>
2023-03-06 09:35:12 -0700

    added main, 404 handler

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/labstack/echo/v4"
+	"music-metrics-back/handler"
+)
+
+func main() {
+
+	e := echo.New()
+
+	e.HTTPErrorHandler = handler.HandleNotFound
+	e.Static("/", "../music-metrics-front/public")
+
+	e.Logger.Fatal(e.Start(":1323"))
+
+}
