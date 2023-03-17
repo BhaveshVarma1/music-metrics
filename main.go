d8b2f48c29b027e17c1b3e91788fa86c5a81e43d
pratt <prattnj@gmail.com>
2023-03-16 22:42:51 -0600

    added update code endpoint (untested)

--- main.go
+++ main.go
@@ -2,12 +2,18 @@ package main
 
 import (
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"music-metrics-back/handler"
 )
 
 func main() {
 
 	e := echo.New()
+
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"https://dev.musicmetrics.app:443"},
+		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAuthorization},
+	}))
 
 	e.POST("/updateCode", handler.HandleUpdateCode)
 
