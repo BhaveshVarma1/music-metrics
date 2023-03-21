d867e247548b188d896320c7ed6e103a6d5a8883
pratt <prattnj@gmail.com>
2023-03-20 18:43:38 -0600

    added get avg year (testing)

--- service/stats.go
+++ service/stats.go
@@ -1,23 +1,30 @@
 package service
 
-import "music-metrics-back/dal"
+import (
+	"fmt"
+	"music-metrics-back/dal"
+)
 
 func GetAverageYear(username string) int {
 
 	tx, db, err := dal.BeginTX()
 	if err != nil {
+		fmt.Println(err)
 		return -1
 	}
 
 	result, err := dal.GetAverageYear(tx, username)
 	if err != nil {
+		fmt.Println(err)
 		if dal.CommitAndClose(tx, db, false) != nil {
+			fmt.Println("yo")
 			return -1
 		}
 		return -1
 	}
 
 	if dal.CommitAndClose(tx, db, true) != nil {
+		fmt.Println("yo yo")
 		return -1
 	}
 
