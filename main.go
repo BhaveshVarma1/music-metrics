f303f429cfad2f67218a33c466a1774f496eaf7b
pratt <prattnj@gmail.com>
2023-03-29 12:37:37 -0600

    trying to debug new project rewrite

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"music-metrics/handler"
@@ -23,10 +22,10 @@ func main() {
 	e.GET("/averageYear/:username", handler.HandleAverageYear)
 	e.GET("/songCounts/:username", handler.HandleSongCounts)
 
-	e.GET("/static/*", func(c echo.Context) error {
+	/*e.GET("/static/*", func(c echo.Context) error {
 		fmt.Println("Serving static file")
 		return c.File(buildPath + c.Request().URL.Path)
-	})
+	})*/
 
 	e.GET("/manifest.json", func(c echo.Context) error {
 		return c.File(buildPath + "/manifest.json")
