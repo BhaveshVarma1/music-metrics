346121ea224973b07e07cb226e47c90d89539bda
pratt <prattnj@gmail.com>
2023-03-29 13:48:58 -0600

    trying to debug new project rewrite

--- testEnv.go
+++ testEnv.go
@@ -8,4 +8,10 @@ import (
 func main() {
 	fmt.Println(os.Getenv("MYSQL_PASSWORD"))
 	fmt.Println(os.Getenv("SPOTIFY_CLIENT_SECRET"))
+	_, err := os.Stat("public/build/manifest.json")
+	if os.IsNotExist(err) {
+		fmt.Println("File does not exist")
+	} else {
+		fmt.Println("File exists")
+	}
 }
