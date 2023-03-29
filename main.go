16c55801139c09198c0fcd4f65a8c1374318b4a2
pratt <prattnj@gmail.com>
2023-03-29 13:49:51 -0600

    trying to debug new project rewrite

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
-	"music-metrics/handler"
 )
 
 func main() {
@@ -19,7 +18,7 @@ func main() {
 		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAuthorization},
 	}))
 
-	e.POST("/updateCode", handler.HandleUpdateCode)
+	/*e.POST("/updateCode", handler.HandleUpdateCode)
 	e.GET("/averageYear/:username", handler.HandleAverageYear)
 	e.GET("/songCounts/:username", handler.HandleSongCounts)
 
@@ -27,16 +26,16 @@ func main() {
 		fmt.Println("Serving static file")
 		fmt.Println(buildPath + c.Request().URL.Path)
 		return c.File(buildPath + c.Request().URL.Path)
-	})
+	})*/
 
 	e.GET("/manifest.json", func(c echo.Context) error {
 		fmt.Println("Serving manifest.json")
 		return c.File(buildPath + "/manifest.json")
 	})
 
-	e.GET("/*", func(c echo.Context) error {
+	/*e.GET("/*", func(c echo.Context) error {
 		return c.File(buildPath + "/index.html")
-	})
+	})*/
 
 	e.Logger.Fatal(e.Start(":3000"))
 
