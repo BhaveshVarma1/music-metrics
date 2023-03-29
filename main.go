9406c9c162fa05eca637b18c81470e6e6fbb1b88
pratt <prattnj@gmail.com>
2023-03-28 20:52:20 -0600

    added static files

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"music-metrics-back/handler"
@@ -23,18 +24,22 @@ func main() {
 	e.GET("/songCounts/:username", handler.HandleSongCounts)
 
 	e.GET("/static/*", func(c echo.Context) error {
+		fmt.Println("Returning file at: " + buildPath + c.Request().URL.Path)
 		return c.File(buildPath + c.Request().URL.Path)
 	})
 
 	e.GET("/manifest.json", func(c echo.Context) error {
+		fmt.Println("Returning manifest.json")
 		return c.File(buildPath + "/manifest.json")
 	})
 
 	e.GET("/favicon.ico", func(c echo.Context) error {
+		fmt.Println("Returning favicon.ico")
 		return c.File(buildPath + "/favicon.ico")
 	})
 
 	e.GET("/*", func(c echo.Context) error {
+		fmt.Println("Returning index.html")
 		return c.File(buildPath + "/index.html")
 	})
 
