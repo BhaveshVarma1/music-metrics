2c13eb4d50d5ec2db2c66b2a3d254f6e5c1248b4
pratt <prattnj@gmail.com>
2023-04-02 14:31:53 -0600

    added table selector (testing)

--- public/src/stats/stats.js
+++ public/src/stats/stats.js
@@ -83,7 +83,7 @@ export function Stats() {
 
     return (
         <div>
-            <PrimaryInfo onClick={() => tempClick} text="Stats central."/>
+            <div onClick={tempClick}><PrimaryInfo text="Stats central."/></div>
             <SecondaryInfo text={"Average release year: " + averageYear}/>
             <div className={'selector'}>
                 <div className={songStyle + ' selector-option corner-rounded-left'} onClick={setToSong}>Top Songs</div>
