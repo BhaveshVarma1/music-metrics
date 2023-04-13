a86b9ea8a622f235d1e08b53e36e2562fd720c8d
pratt <prattnj@gmail.com>
2023-04-13 14:26:18 -0600

    styling re-added decade pie chart

--- public/src/stats/stats.js
+++ public/src/stats/stats.js
@@ -486,7 +486,6 @@ function DecadePieChart() {
     return (
         <div>
             <Chart
-                height={'25rem'}
                 chartType="PieChart"
                 data={chartData}
                 options={{
