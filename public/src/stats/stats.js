415c3fd6993ef3f2fead78f5ddfffdd61e3d373d
pratt <prattnj@gmail.com>
2023-04-13 22:37:51 -0600

    styling hour chart

--- public/src/stats/stats.js
+++ public/src/stats/stats.js
@@ -552,7 +552,7 @@ function HourChart() {
                         },
                         chartArea: {
                             left: 50, // adjust the left margin to make space for the y-axis labels
-                            top: 20, // adjust the top margin to make space for the x-axis labels
+                            top: 0, // adjust the top margin to make space for the x-axis labels
                             width: '100%', // adjust the width to make the chart larger
                             height: '60%' // adjust the height to make the chart larger
                         }
