7ea80b189b48d096913e8d3d66c965885d0d6807
pratt <prattnj@gmail.com>
2023-05-23 16:59:40 -0600

    playing with react-dropzone css

--- public/src/account/account.js
+++ public/src/account/account.js
@@ -99,7 +99,11 @@ function Dropzone() {
                     </li>
                 ))}
             </ul>
-            {files.length !== 0 && <div className={'login-button dropzone-submit'} onClick={submit}><b>SUBMIT</b></div>}
+            {files.length !== 0 && (
+                <div className={'dropdown-submit-wrapper'}>
+                    <div className={'login-button dropzone-submit'} onClick={submit}><b>SUBMIT</b></div>
+                </div>
+            )}
         </div>
     )
 }
