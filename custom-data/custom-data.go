f3e114f8ab4df17f2e428645e922c86ccaa70179
Noah Pratt <prattnj@gmail.com>
2023-11-27 13:27:09 -0700

    Refactor packages

---
+++ custom-data/custom-data.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"os"
+)
+
+func main() {
+
+	filename := os.Args[1]
+	if filename == "" {
+		return
+	}
+
+}
---
+++ track/tracking-script.go
@@ -0,0 +1,9 @@
+package main
+
+import "music-metrics/service"
+
+func main() {
+
+	service.StartTracking()
+
+}
